247ec41d7b87101a4b3103fa86d9a08f4073efc7
OpheliaDanso <mayaafreh@gmail.com>
2024-04-10 09:39:07 +0200

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-[100%] bg-yellow-200 text-black text-center p-4 ">
+    <footer className="w-[100%] bg-slate-800 text-white text-center p-4 ">
       <div className="mb-2">
         <a href="https://twitter.com" className="mx-2">
           Twitter
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import cinemaImage from "../assets/cinema.png";
 
 const Movie = ({ movies }) => {
   const { movieTitle } = useParams();
@@ -11,22 +12,55 @@ const Movie = ({ movies }) => {
   }
 
   return (
-    <div className=" flex-1 grid grid-cols-2  gap-8">
-      <img
-        src={movie.fields.imageOfMovie[0].fields.file.url}
-        alt=""
-        width="300"
-      />
-      <div className="flex flex-col gap-10">
-        <h1>{movie.fields.title}</h1>
-        <h2>
-          {movie.fields.actors[0].fields.lastName}{" "}
-          {movie.fields.actors[0].fields.name}
-        </h2>
-        <p>{movie.fields.year}</p>
-        <p>{movie.fields.duration}</p>
-        <p>{movie.fields.category}</p>
-        <p>{movie.fields.descript}</p>
+    <div
+      style={{
+        backgroundImage: `url(${cinemaImage})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className=" flex-1 grid grid-cols-1 md:grid-cols-2 gap-8 mt-20 mb-20">
+        <img
+          className="ml-16"
+          src={movie.fields.imageOfMovie[0].fields.file.url}
+          alt=""
+          width="600"
+        />
+        <div className="flex flex-col gap-10">
+          <h1 className="font-semibold text-white">
+            Title: {movie.fields.title}
+          </h1>
+          <h2 className="font-semibold text-white">
+            {" "}
+            Principal Actor: {""}
+            {movie.fields.actors[0].fields.lastName}{" "}
+            {movie.fields.actors[0].fields.name}
+          </h2>
+          <p className="font-semibold text-white">Year: {movie.fields.year}</p>
+          <p className="font-semibold text-white">
+            Duration: {movie.fields.duration}
+          </p>
+          <p className="font-semibold text-white">
+            Category: {movie.fields.category}
+          </p>
+          <p className="font-semibold text-white">
+            Summary:
+            <br />
+            <br />
+            {movie.fields.descript}
+          </p>
+          <div className="flex gap-8">
+            <button className="rounded bg-transparent hover:bg-blue-500 w-52 h-10 text-white">
+              READY TO WATCH!
+            </button>
+            <button className="rounded bg-transparent hover:bg-blue-500 w-52 h-10 text-white">
+              ADD TO PLAYLIST
+            </button>
+            <button className="rounded bg-transparent hover:bg-blue-500 w-52 h-10 text-white">
+              MARK AS NEXT
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
